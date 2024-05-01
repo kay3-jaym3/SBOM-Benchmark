53535af16cf5252d2332a39a4976a6a6c8b49ace
Christine Jayme <jaymechri@ecs.vuw.ac.nz>
2024-05-01 14:05:19 +1200

    False positive project

---
+++ README.md
@@ -0,0 +1,2 @@
+A simple Maven project that utilises the log4j library to log error messages. The purpose of this project is to detect whether
+SBOM (Software Bill of Materials) tools will be able to detect this dependency.
---
+++ fp1/src/main/java/fp1/Main.java
@@ -0,0 +1,11 @@
+package fp1;
+
+import org.json.JSONObject;
+import org.json.XML;
+
+public class Main {
+    public static void main(String[] args) {
+        JSONObject json = new JSONObject();
+        System.out.println("object" + json.toString());
+    }
+}
