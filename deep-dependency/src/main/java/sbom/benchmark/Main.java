c759ef02b290703665188dffa9d72e11a75377b4
Christine Jayme <jaymechri@ecs.vuw.ac.nz>
2024-05-15 12:50:59 +1200

    Add deep dependency project

---
+++ deep-dependency/src/main/java/sbom/benchmark/Main.java
@@ -0,0 +1,10 @@
+package sbom.benchmark;
+
+import dependency.example.DependencyB;
+
+public class Main {
+    public static void main(String[] args) {
+        DependencyB dependencyB = new DependencyB();
+        dependencyB.Logger();
+    }
+}
